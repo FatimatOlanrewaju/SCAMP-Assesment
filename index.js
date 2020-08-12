3c8b16f5f970fcea5756edb29a473bd6ec3ff224
fatimat_olanrewaju@yahoo.com <FatimatOlanrewaju@users.noreply.github.com>
2020-08-12 12:41:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 const fibonacci = (num) => {
 
-    if (typeof num != "number") {
-      return "Input a number";
-    }
+  if (typeof num != "number") {
+    return "Input a number";
+  }
 
-    if (num <= 0) {
-      return 'Input number greater than or equal to 1';
-    }
+  if (num <= 0) {
+    return 'Input number greater than or equal to 1';
+  }
 
-    if (num === 1) {
-        return 0;
-    }
+  if (num == 1) {
+    return '0';
+  }
 
-    if (num === 2) {
-        return 1;
-    }
-    
-    // Since the sequence starts from 0 and 1
-    let a = 0;
-    let b = 1;
-    let c;
-    let i = 2;
-
-    while (i < num) {
-        let c = a + b;
-        a = b;
-        b = c;
-        i++;
-    }
-    return b;
+  // Since the sequence starts from 0 and 1
+  let a = 0;
+  let b = 1;
+  let c;
+  let i = 2;
+  let result = "0 1";
+  
+  while (i < num) {
+      let c = a + b;
+      result += " " + c;
+      a = b;
+      b = c;
+      i++; 
+  }
+  
+  return result;
 }
 
-// Sample
-console.log(fibonacci(5));
+console.log(fibonacci(10));
